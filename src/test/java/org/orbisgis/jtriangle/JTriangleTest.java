7093802f4bbf1bfa72c898ea64fca2c6906de9b0
nicolas-f <github@nettrader.fr>
2017-01-13 10:43:29 +0100

    First commit, push triangle src

---
+++ src/test/java/org/orbisgis/jtriangle/JTriangleTest.java
@@ -0,0 +1,19 @@
+package org.orbisgis.jtriangle;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Example of using a compiled C function from Java
+ */
+public class JTriangleTest {
+
+  @Test
+  public void simplePointMesh() {
+
+    assertThat(true);
+  }
+
+}
